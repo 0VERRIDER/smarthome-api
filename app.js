d9ae8d767eb1cf9794178681695c0045877b723d
0VERRIDER <anshilp@karunya.edu.in>
2021-08-02 22:49:36 +0530

    Formatted

--- app.js
+++ app.js
@@ -1,47 +1,45 @@
-const express = require("express")
-const mongoose  = require("mongoose")
-const app = express()
-const createRoute = require("./api/routes/create")
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+const createRoute = require("./api/routes/create");
 //body parser
-app.use(express.urlencoded({extended:false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 //MongoDB call
-try{
-    mongoose.connect(process.env.MONGODB_URL,{
-        useNewUrlParser: true ,
-        useUnifiedTopology: true
-    })
-    mongoose.set('useCreateIndex', true);
-    }
-    catch(error){
-        app.use((req,res,next) =>{
-            res.status(500).json({
-                error:{
-                    message : "Something went wrong!",
-                    details : error
-                }
-            });
+try {
+    mongoose.connect(process.env.MONGODB_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    });
+    mongoose.set("useCreateIndex", true);
+} catch (error) {
+    app.use((req, res, next) => {
+        res.status(500).json({
+            error: {
+                message: "Something went wrong!",
+                details: error,
+            },
+        });
         handleError(error);
     });
-    }
-//routes 
-app.use("/api/create",createRoute)
+}
+//routes
+app.use("/api/create", createRoute);
 //error handlers
-app.use((req,res,next) =>{
+app.use((req, res, next) => {
     const error = new Error("Not Found");
     error.status = 404;
     next(error); // forwards the error request
 });
 
-
-app.use((error,req,res,next) =>{
-    res.status(error.status || 500 );
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
     res.json({
-        error:{
-            message : error.message
-        }
+        error: {
+            message: error.message,
+        },
     });
 });
 
-module.exports = app
+module.exports = app;
