1290d5b47bad67e249ab325f87fabc897e112fca
0VERRIDER <anshilp@karunya.edu.in>
2021-08-02 22:40:39 +0530

    Updated : create module file path

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require("express")
 const app = express()
-const createRoute = require("./api/create")
+const createRoute = require("./api/routes/create")
 //body parser
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
