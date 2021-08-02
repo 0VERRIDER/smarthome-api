e6100c40a49c0b328e43046b161ff3a0a892e58c
0VERRIDER <anshilp@karunya.edu.in>
2021-08-02 22:40:17 +0530

    Update : changed the file path

--- api/routes/create.js
+++ api/routes/create.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router   = express.Router();
 
-router.get('/',(req,res,next)=>{
+router.post('/',(req,res,next)=>{
 res.status(200).json({message:"hi there"});
 })
+
+module.exports = router;
