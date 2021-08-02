aeb0b395a03c65ace347fd8f67316c6536c6ce3c
0VERRIDER <anshilp@karunya.edu.in>
2021-08-02 22:15:01 +0530

    Added : Created node server

---
+++ server.js
@@ -0,0 +1,8 @@
+const dotenv = require("dotenv")
+dotenv.config()
+const http = require("http")
+const PORT = process.env.PORT || 3000
+const app = require("./app")
+const server = http.createServer(app)
+
+server.listen(PORT)
