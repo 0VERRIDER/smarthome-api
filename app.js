18316962dc36a53a69bf5489e7f7c697022f3514
0VERRIDER <anshilp@karunya.edu.in>
2021-08-02 22:18:17 +0530

    Added : Error Handlers

--- app.js
+++ app.js
@@ -4,6 +4,21 @@ const app = express()
 app.use(express.urlencoded({extended:false}));
 app.use(express.json());
 
+//error handlers
+app.use((req,res,next) =>{
+    const error = new Error("Not Found");
+    error.status = 404;
+    next(error); // forwards the error request
+});
 
+
+app.use((error,req,res,next) =>{
+    res.status(error.status || 500 );
+    res.json({
+        error:{
+            message : error.message
+        }
+    });
+});
 
 module.exports = app
