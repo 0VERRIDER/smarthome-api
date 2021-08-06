db296a60003b73a687781bab32f9c6fe34bc5446
0VERRIDER <anshilp@karunya.edu.in>
2021-08-06 20:29:49 +0530

    Created : API create Method

---
+++ api/routes/api_create.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router   = express.Router();
+const API = require('../models/api');
+const mongoose = require('mongoose');
+
+router.post('/',(req,res,next)=>{
+    const api = new API({
+        _id: mongoose.Types.ObjectId(),
+        api_name: req.body.api_name,
+        registred_email : req.body.email,
+        password: req.body.password,
+        api_url : req.body.url,
+        api_key : req.body.api_key,
+        req_type: req.body.type 
+    });
+    api.save().then(result => {
+       
+        return res.status(201).json({
+            message : "Thank you API registred successfully.",
+            result: result
+        });
+    }).catch( err =>{
+        try
+        {res.status(409).json({
+            message : err.keyPattern['api_name']>0? "User already exist" : "Invalid entries found",
+        
+        });
+       
+}
+catch(err){
+    res.status(403).json({
+            error: "Invalid entries found"
+        });
+    }
+    
+    });
+
+
+
+})
+
+module.exports = router;
