b5130e6e9307ac45e0741abf69c6d641f9e23dc7
0VERRIDER <anshilp@karunya.edu.in>
2021-08-06 23:19:11 +0530

    Implemented : Auth system

---
+++ api/auth/auth.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+module.exports = (req,res,next)=>{
+    try{    
+        const token = req.cookies.token;
+        const decoded = jwt.verify(token,process.env.AUTH_SECRET,null);
+        req.userData = decoded;
+        next();
+}
+catch(err){
+    return res.status(401).json({
+        message : "Authentication Error"
+    });
+}
+}
