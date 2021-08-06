62021f33d32a43bb546e23927918aa99da0ead75
0VERRIDER <anshilp@karunya.edu.in>
2021-08-06 20:18:58 +0530

    Changed : http to https protocol

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const dotenv = require("dotenv")
 dotenv.config()
-const http = require("http")
+const http = require("https")
 const PORT = process.env.PORT || 3000
 const app = require("./app")
 const server = http.createServer(app)
